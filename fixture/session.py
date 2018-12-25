d241628c9c423b7284637490a12d3c94f0f30fbc
Pierre <pstoiko@gmail.com>
2018-12-25 12:53:14 -0500

    Новое учебное приложение: баг-трекер Mantis | Done

--- fixture/session.py
+++ fixture/session.py
@@ -15,7 +15,7 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user")
+#        wd.find_element_by_name("username")
 
     def ensure_logout(self):
         wd = self.app.wd
