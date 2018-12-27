180c01938474b05fa80c3b7b02f44bae03318684
Pierre <pstoiko@gmail.com>
2018-12-27 12:58:40 -0500

    Finished to add all methods for project. Test failed. Doesn't take list

--- fixture/project.py
+++ fixture/project.py
@@ -2,95 +2,11 @@
 
 from model.project import Project
 
+
 class ProjectHelper:
-    from selenium.webdriver.support.select import Select
 
     def __init__(self, app):
         self.app = app
-
-    def submit_contact_form(self):
-        wd = self.app.wd
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Notes:'])[1]/following::input[1]").click()
-
-    def fill_in_contact_form(self, contact):
-        wd = self.app.wd
-        self.open_add_new_contact_page()
-        self.fill_contact_form(contact)
-        self.specify_drop_downs(contact)
-        self.submit_contact_form()
-        self.app.return_on_home_page()
-
-    def specify_drop_downs(self, contact):
-        wd = self.app.wd
-        self.select_in_drop_down('bday', contact.birthday)
-        self.select_in_drop_down('bmonth', contact.birth_month)
-
-    def select_in_drop_down(self, field_name, text):
-        wd = self.app.wd
-        if text is not None:
-            Select(wd.find_element_by_name(field_name)).select_by_visible_text(text)
-
-    def fill_contact_form(self, contact):
-        wd = self.app.wd
-        self.change_field_value('firstname', contact.firstname)
-        self.change_field_value('middlename', contact.middlename)
-        self.change_field_value('lastname', contact.lastname)
-
-    def change_field_value(self, field_name, text):
-        wd = self.app.wd
-        if text is not None:
-            wd.find_element_by_name(field_name).click()
-            wd.find_element_by_name(field_name).clear()
-            wd.find_element_by_name(field_name).send_keys(text)
-
-    def open_add_new_contact_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def delete_first_contact(self):
-        wd = self.app.wd
-        self.select_first_contact()
-        wd.find_element_by_xpath("//input[@value='Delete']").click()
-        wd.switch_to.alert.accept()
-
-    def select_first_contact(self):
-        wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
-
-    def modify_first_contact(self, new_contact_data):
-        wd = self.app.wd
-        self.select_first_contact()
-        # click modification button
-        wd.find_element_by_css_selector("img[alt=\"Edit\"]").click()
-        # specify new data
-        self.fill_contact_form(new_contact_data)
-        self.specify_drop_downs(new_contact_data)
-        # click update button
-        wd.find_element_by_name("update").click()
-        # return to home page
-        self.app.return_on_home_page()
-
-    def count(self):
-        wd = self.app.wd
-        self.open_home_page()
-        return len(wd.find_elements_by_name("selected[]"))
-
-    def open_home_page(self):
-        wd = self.app.wd
-        wd.get("http://localhost/addressbook/")
-
-    def get_contact_list(self):
-        wd = self.app.wd
-        self.open_home_page()
-        contacts = []
-        for row in wd.find_elements_by_name("entry"):
-            cells = row.find_elements_by_tag_name('td')
-            firstname = cells[1].text
-            lastname = cells[2].text
-            id = cells[0].find_element_by_name("selected[]").get_attribute('value')
-            contacts.append(Contact(firstname=firstname, lastname=lastname, id=id))
-        return contacts
 
     def open_manage_projects_page(self):
         wd = self.app.wd
@@ -99,15 +15,33 @@ class ProjectHelper:
 
     def get_project_list(self):
         wd = self.app.wd
-        self.open_manage_projects_page()
         projects = []
-        for row in wd.find_elements_by_name("row-1"):
+        for row in wd.find_elements_by_name("row-1") or wd.find_elements_by_name("row-2"):
             cells = row.find_elements_by_tag_name('td')
             name = cells[0].text
-            id = cells[0].find_element_by_name("selected[]").get_attribute('value')
-            contacts.append(Contact(firstname=firstname, lastname=lastname, id=id))
-        return contacts
+            #id = cells[0].find_element_by_name("selected[]").get_attribute('value')
+            projects.append(Project(name=name))
+        return projects
 
+    def add_project(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//input[@value='Create New Project']").click()
+
+    def fill_in_project_form(self, project):
+        wd = self.app.wd
+        wd.find_element_by_name("name").click()
+        wd.find_element_by_name("name").clear()
+        wd.find_element_by_name("name").send_keys(project.name)
+        self.submit_project_form()
+        self.return_to_manage_projects_page()
+
+    def submit_project_form(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//input[@value='Add Project']").click()
+
+    def return_to_manage_projects_page(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//a[contains(text(),'Proceed')]").click()
 
 
 
