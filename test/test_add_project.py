3b9e94c4d95e26ac7c8c96f64aabbeee9829cc6d
Pierre <pstoiko@gmail.com>
2018-12-27 00:14:06 -0500

    Prepared structure of the test_add_project + edited it

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -3,8 +3,8 @@ from model.project import Project
 
 def test_add_project(app):
     app.session.login()
-    old_projects = app.project.get_project_list()
     app.project.open_manage_projects_page()
+    old_projects = app.project.get_project_list()
     app.project.add_project()
     app.contact.fill_in_project_form(Project(name='Name'))
     new_projects = app.project.get_project_list()
