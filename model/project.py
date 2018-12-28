219ecb3ac1fc79ff96b9c38f7b382ff5c32cff3d
Pierre <pstoiko@gmail.com>
2018-12-28 12:18:50 -0500

    test_add_project failed with two verifications

--- model/project.py
+++ model/project.py
@@ -2,22 +2,21 @@ from sys import maxsize
 
 
 class Project:
-    def __init__(self, name=None, status=None, enabled=None, view_status=None, description=None, id=None):
+    def __init__(self, name=None, status=None, enabled=None, view_status=None, description=None):
         self.name = name
         self.status = status
         self.enabled = enabled
         self.view_status = view_status
         self.description = description
-        self.id = id
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+        return self.name == other.name
 
-    def id_or_max(self):
+    '''def id_or_max(self):
         if self.id:
             return int(self.id)
         else:
-            return maxsize
+            return maxsize'''
 
     def __repr__(self):
-        return "%s:%s;%s;%s" % (self.id, self.name, self.status, self.enabled)
+        return "%s:%s;%s" % (self.name, self.status, self.enabled)
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -6,7 +6,10 @@ def test_add_project(app):
     app.project.open_manage_projects_page()
     old_projects = app.project.get_project_list()
     app.project.add_project()
-    app.project.fill_in_project_form(Project(name='Name13'))
+    project = Project(name='Name24')
+    app.project.fill_in_project_form(project)
     new_projects = app.project.get_project_list()
     assert len(old_projects) + 1 == len(new_projects)
+    old_projects.append(project)
+    assert sorted(old_projects, key=project.name) == sorted(new_projects, key=project.name)
 
