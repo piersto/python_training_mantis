2a38e4d2d5ebd455fb67983fff2300a4063a3c9e
piersto <pstoiko@gmail.com>
2019-01-02 17:15:42 -0500

    Added random choice to the test_add_project

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,12 +1,27 @@
 from model.project import Project
+import pytest
+import random
+import string
 
 
-def test_add_project(app):
+def random_string(prefix, maxlen):
+    # string will be chosen from letters, digits and 10 spaces -- ' '*10
+    symbols = string.ascii_letters + string.digits + ' '*5
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    # will generate random string that starts with word 'Name' or 'Header etc and + some more random symbols
+    Project(name=random_string('Name', (10)))
+    ]
+
+
+@pytest.mark.parametrize('project', testdata, ids=[repr(x) for x in testdata])
+def test_add_project(app, project):
     app.session.login('administrator', 'root')
     app.project.open_manage_projects_page()
     old_projects = app.project.get_project_list()
     app.project.add_project()
-    project = Project(name='Name24')
     app.project.fill_in_project_form(project)
     new_projects = app.project.get_project_list()
     assert len(old_projects) + 1 == len(new_projects)
